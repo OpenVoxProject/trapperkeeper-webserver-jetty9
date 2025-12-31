a65d04e615eb373729423804855b5136fa00d6d0
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-12-30 20:03:22 -0800

    Fix unsupported Java version message

--- project.clj
+++ project.clj
@@ -99,7 +99,7 @@
                                  ;; that sets up the JVM classpaths during installation.
                                  :jvm-opts ~(let [version (System/getProperty "java.version")
                                                   [major minor _] (clojure.string/split version #"\.")
-                                                  unsupported-ex (ex-info "Unsupported major Java version. Expects 11 or 17."
+                                                  unsupported-ex (ex-info "Unsupported major Java version. Expects 17 or 21."
                                                                    {:major major
                                                                     :minor minor})]
                                               (condp = (java.lang.Integer/parseInt major)
