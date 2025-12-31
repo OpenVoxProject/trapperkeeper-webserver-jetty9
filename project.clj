e228b10b43defe7d4b9f186d500cecb871d57c5a
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-12-30 17:58:08 -0800

    Set versions to releases and update deploy repo

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
 
   :min-lein-version "2.9.1"
 
-  :parent-project {:coords [org.openvoxproject/clj-parent "7.4.1-SNAPSHOT"]
+  :parent-project {:coords [org.openvoxproject/clj-parent "7.5.0"]
                    :inherit [:managed-dependencies]}
 
   ;; Abort when version ranges or version conflicts are detected in
@@ -49,9 +49,9 @@
   :java-source-paths  ["java"]
 
   :plugins [[lein-parent "0.3.7"]
-            [org.openvoxproject/i18n "0.9.3-SNAPSHOT"]]
+            [org.openvoxproject/i18n "0.9.3"]]
 
-  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :username :env/CLOJARS_USERNAME
                                      :password :env/CLOJARS_PASSWORD
                                      :sign-releases false}]]
